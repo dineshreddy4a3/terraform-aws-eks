d8f819d8f3d32518f535c638cddb76b2b7c17f77
Dinesh <dinesh.reddy165@gmail.com>
2026-03-22 10:28:36 +0100

    add lock file

--- 30-db/provider.tf
+++ 30-db/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "daws78s-remote-state"
+    bucket = "myeksbucket-remote-state"
     key    = "expense-dev-db"
     region = "us-east-1"
-    dynamodb_table = "daws78s-locking"
+    use_lockfile   = true 
   }
 }
 
